391e125cf2900b3ffc51cf01d49eac01ba35e451
acerstructor <acerstructorofficial@gmail.com>
2025-06-28 11:22:55 +0800

    created: headers.h for including all needed header files

---
+++ src/headers.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <ctype.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+#ifdef XINERAMA
+#include <X11/extensions/Xinerama.h>
+#endif
+#include <X11/Xft/Xft.h>
