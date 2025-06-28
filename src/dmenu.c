49eae9f5ff46efcf75f7904ba9e2551b686284d9
acerstructor <acerstructorofficial@gmail.com>
2025-06-28 11:23:34 +0800

    modified: moved all included headers into headers.h

--- src/dmenu.c
+++ src/dmenu.c
@@ -1,21 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-#include <ctype.h>
-#include <locale.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <time.h>
-#include <unistd.h>
-
-#include <X11/Xlib.h>
-#include <X11/Xatom.h>
-#include <X11/Xutil.h>
-#ifdef XINERAMA
-#include <X11/extensions/Xinerama.h>
-#endif
-#include <X11/Xft/Xft.h>
-
+#include "headers.h"
 #include "drw.h"
 #include "util.h"
 
