73a23dbb941d424ef0cb8917ab4f948e1f9c3094
acerstructor <acerstructorofficial@gmail.com>
2025-08-11 23:25:43 +0800

    modified: made the brightness of the border color a little bit higher

--- dmenu/src/dmenu.c
+++ dmenu/src/dmenu.c
@@ -57,7 +57,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#f8f8f2", "#0A0A0A" },
-	[SchemeSel] = { "#0A0A0A", "#dae5c8" },
+	[SchemeSel] = { "#0A0A0A", "#d2d2b5" },
 	[SchemeOut] = { "#0A0A0A", "#f8f8f2" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
